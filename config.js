a4d4355b99a38444e743bff4ee2c57e0bbd7f4ab
ShriKrishnaMahalAvp <shrikrishnamahalavp@gmail.com>
2026-03-02 19:41:31 +0530

    Add configuration file for sensitive URLs

---
+++ config.js
@@ -0,0 +1,11 @@
+// Configuration file - contains sensitive URLs
+// Real secrets are loaded here, DO NOT commit actual URLs to GitHub
+
+const CONFIG = {
+    // Gallery Config - Google Apps Script URL
+    galleryUrl: 'https://script.google.com/macros/s/AKfycby7edmZEdORBU1V0bSrEXVgburwsEyT0McVst3OZFxJOQbzVYu26DoU-38FPIVKdA3I/exec',
+    
+    // Calendar Config - Google Calendar public ICS URL
+    calendarTitle: "Availability Checker - Shri Krishna Mahal",
+    calendarIcsUrl: "https://calendar.google.com/calendar/ical/f7beb684c4719068f5e4886e5da04969ea34a1b266eebae19781b361b513bca9%40group.calendar.google.com/public/basic.ics"
+};
